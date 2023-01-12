a12662098f7ca84933896b8c2f7267a9045ecf18
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2023-01-12 23:03:48 +0900

    fix for ver2 dataset (#3)

--- tests/wrime_test.py
+++ tests/wrime_test.py
@@ -27,3 +27,28 @@ def test_load_dataset(
     assert dataset["train"].num_rows == expected_train_num_rows  # type: ignore
     assert dataset["validation"].num_rows == expected_val_num_rows  # type: ignore
     assert dataset["test"].num_rows == expected_test_num_rows  # type: ignore
+
+    writer_readers = [
+        "writer",
+        "reader1",
+        "reader2",
+        "reader3",
+        "avg_readers",
+    ]
+    expected_keys = ["sentence", "user_id", "datetime"] + writer_readers
+
+    for split in ["train", "validation", "test"]:
+        split_dataset = dataset[split]  # type: ignore
+
+        for data in split_dataset:
+            assert len(data.keys()) == len(expected_keys)
+            for expected_key in expected_keys:
+                assert expected_key in data.keys()
+
+            for k in writer_readers:
+                if dataset_name == "ver1":
+                    assert len(data[k]) == 8  # 8 感情強度
+                elif dataset_name == "ver2":
+                    assert len(data[k]) == 8 + 1  # 8 感情強度 + 1 感情極性
+                else:
+                    raise ValueError(f"Invalid dataset version: {dataset_name}")
