ff2b4add87ed992ed835a8dd384c0061a20e4963
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2023-01-12 10:46:27 +0900

    add files for tests

---
+++ tests/wrime_test.py
@@ -0,0 +1,30 @@
+import datasets as ds
+import pytest
+
+
+@pytest.fixture
+def dataset_path() -> str:
+    return "wrime.py"
+
+
+@pytest.mark.parametrize(
+    "dataset_name, expected_train_num_rows, expected_val_num_rows, expected_test_num_rows,",
+    (
+        ("ver1", 40000, 1200, 2000),
+        ("ver2", 30000, 2500, 2500),
+    ),
+)
+def test_load_dataset(
+    dataset_path: str,
+    dataset_name: str,
+    expected_train_num_rows: int,
+    expected_val_num_rows: int,
+    expected_test_num_rows: int,
+):
+
+    dataset = ds.load_dataset(path=dataset_path, name=dataset_name)
+    breakpoint()
+
+    assert dataset["train"].num_rows == expected_train_num_rows  # type: ignore
+    assert dataset["validation"].num_rows == expected_val_num_rows  # type: ignore
+    assert dataset["test"].num_rows == expected_test_num_rows  # type: ignore
